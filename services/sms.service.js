d6fbf0a34d2b06b348d74a135f6947204d9f464f
murilo <macquadros@gmail.com>
2016-10-21 09:02:32 -0200

    feat(sms-service): list

--- services/sms.service.js
+++ services/sms.service.js
@@ -4,11 +4,14 @@ var RequestConfig = require('../config/request.config');
 var Interceptor = require('../config/interceptor.config');
 var endpoint = require('../config/env.config').endpoint;
 var sendSmsPath = 'v2/sms';
+var devicesPath = 'devices/';
+var listSmsPath = '/sms';
+var queryString = require('./query.service');
 
 function SMSService() {
 }
 
-SMSService.prototype.sendSMS = function(smsInfo){
+SMSService.prototype.sendSMS = function (smsInfo) {
     var smsEndpoint = endpoint + sendSmsPath;
 
     return req(RequestConfig.generateOptions(RequestConfig.POST, smsEndpoint, smsInfo))
@@ -18,6 +21,28 @@ SMSService.prototype.sendSMS = function(smsInfo){
             Intercecptor.callInterceptor(err);
             throw err;
         });
+};
+
+SMSService.prototype.list = function (device, initialDate, finalDate, initialRangeItem, finalRangeItem) {
+    var queries = queryString.stringify({initialDate: initialDate, finalDate: finalDate});
+    var smsListEndpoint = endpoint + devicesPath + device + listSmsPath;
+    if (queries) {
+        smsListEndpoint += '?' + queries;
+    }
+
+    initialRangeItem = initialRangeItem || '';
+
+    return req(RequestConfig.generateOptions(RequestConfig.GET, smsListEndpoint,
+        null, {range: 'items ' + initialRangeItem + '-' + finalRangeItem})).then(function (response) {
+        return (JSON.parse(response));
+    }, function (err) {
+        Intercecptor.callInterceptor(err);
+        throw err;
+    });
+};
+
+SMSService.prototype.listLasts = function (device, qty) {
+    return this.list(device, null, null, null, qty);
 };
 
 module.exports = SMSService;
